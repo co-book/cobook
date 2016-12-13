eee6d5567baf92fc5fd8c5c0f67d7ebf9a01401f
parlantier <aby41970826@gmail.com>
2016-12-13 15:37:23 +0900

    Update reply.sql
    reply sql수정

--- src/main/resources/SQL/reply.sql
+++ src/main/resources/SQL/reply.sql
@@ -6,14 +6,13 @@ replycont varchar2(2000),-- 댓글 4000바이트
 regdate date default sysdate,-- 등록날짜
 reparent int,-- 댓글부모번호
 redepth int default 0,-- 댓글 레벨
-reorder int default 1-- 댓글 순서
+reorder int default 1,-- 댓글 순서
+user_id varchar2(30)
 );
 
-alter table reply_test add constraint fk_reply_test
-foreign key(replyno) references test_board(bno);
+
 
 create sequence replyno_seq increment by 1;
 
-insert into reply_test(replyno, boardno, replycont, reparent)
-values(replyno_seq.nextval, 27, '하야해', 27)
-
+insert into reply_test(replyno, boardno, replycont, reparent, user_id)
+values(replyno_seq.nextval, 27, '하야해', replyno_seq.currval, '갑동이');
